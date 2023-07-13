5ac715da72c30eccabdaf3c62e732efc824ad412
David Smětala <x213@centrum.cz>
2023-07-14 00:59:39 +0200

    Logic works, graphic stoo. needs to solve input and exceptions

---
+++ logic.py
@@ -0,0 +1,33 @@
+def gamestatus(gamemarks):
+    gamemarks_matrix = [["e", "e", "e"],
+                        ["e", "e", "e"],
+                        ["e", "e", "e"]]
+    gamemarks_for = ["O", "X"] 
+
+    for board_num, board_mark in gamemarks.items():
+        if board_num == 1: gamemarks_matrix[0][0] = board_mark
+        elif board_num == 2: gamemarks_matrix[0][1] = board_mark
+        elif board_num == 3: gamemarks_matrix[0][2] = board_mark
+        elif board_num == 4: gamemarks_matrix[1][0] = board_mark
+        elif board_num == 5: gamemarks_matrix[1][1] = board_mark
+        elif board_num == 6: gamemarks_matrix[1][2] = board_mark
+        elif board_num == 7: gamemarks_matrix[2][0] = board_mark
+        elif board_num == 8: gamemarks_matrix[2][1] = board_mark
+        elif board_num == 9: gamemarks_matrix[2][2] = board_mark
+    #print(gamemarks_matrix)
+    for i in range(2):
+        if gamemarks_matrix[0][0] == gamemarks_for[i] and gamemarks_matrix[0][1] == gamemarks_for[i] and gamemarks_matrix[0][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[1][0] == gamemarks_for[i] and gamemarks_matrix[1][1] == gamemarks_for[i] and gamemarks_matrix[1][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[2][0] == gamemarks_for[i] and gamemarks_matrix[2][1] == gamemarks_for[i] and gamemarks_matrix[2][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[0][0] == gamemarks_for[i] and gamemarks_matrix[1][0] == gamemarks_for[i] and gamemarks_matrix[2][0] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[0][1] == gamemarks_for[i] and gamemarks_matrix[1][1] == gamemarks_for[i] and gamemarks_matrix[2][1] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[0][2] == gamemarks_for[i] and gamemarks_matrix[1][2] == gamemarks_for[i] and gamemarks_matrix[2][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[0][0] == gamemarks_for[i] and gamemarks_matrix[1][1] == gamemarks_for[i] and gamemarks_matrix[2][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
+        if gamemarks_matrix[0][2] == gamemarks_for[i] and gamemarks_matrix[1][1] == gamemarks_for[i] and gamemarks_matrix[2][0] == gamemarks_for[i]: return(False, gamemarks_for[i])
+
+    return(True,"")
+
+
+
+        
+        
--- source.py
+++ source.py
@@ -10,5 +10,28 @@ marks in a:
 * vertical or
 * diagonal row
 ========================================
-Let's start the game"""
+Let's start the game
+If you want to exit press E"""
 
+def gameboard(gamemarks):
+    gameboard = """
+-------------------------
+|1      |2      |3      |
+|       |       |       |
+|       |       |       |
+-------------------------
+|4      |5      |6      |
+|       |       |       |
+|       |       |       |
+-------------------------
+|7      |8      |9      |
+|       |       |       |
+|       |       |       |
+-------------------------
+"""
+    for board_num, board_mark in gamemarks.items():      
+        if board_num in range(1,4): gameboard = gameboard[:(57 + ((board_num - 1) * 8))] + board_mark + gameboard[(57 + ((board_num - 1) * 8))+1:]
+        elif board_num in range(4,7): gameboard = gameboard[:(161 + ((board_num - 4) * 8))] + board_mark + gameboard[(161 + ((board_num - 4) * 8))+1:]
+        elif board_num in range(7,10): gameboard = gameboard[:(265 + ((board_num - 7) * 8))] + board_mark + gameboard[(265 + ((board_num - 7) * 8))+1:]
+
+    print(gameboard)
