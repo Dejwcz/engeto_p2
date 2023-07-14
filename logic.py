90b1e83142e609d352f6bdc9b8e8c42da8685794
David Smětala <x213@centrum.cz>
2023-07-14 10:08:44 +0200

    verification on used numbers does not work, added comments for imports(functions)

--- logic.py
+++ logic.py
@@ -1,3 +1,5 @@
+# rewrites gamemarks to matrix and then decides if there are any "3 in row".
+# if there are "3 in row" returns True value and mark of winner
 def gamestatus(gamemarks):
     gamemarks_matrix = [["e", "e", "e"],
                         ["e", "e", "e"],
@@ -14,7 +16,7 @@ def gamestatus(gamemarks):
         elif board_num == 7: gamemarks_matrix[2][0] = board_mark
         elif board_num == 8: gamemarks_matrix[2][1] = board_mark
         elif board_num == 9: gamemarks_matrix[2][2] = board_mark
-    #print(gamemarks_matrix)
+    
     for i in range(2):
         if gamemarks_matrix[0][0] == gamemarks_for[i] and gamemarks_matrix[0][1] == gamemarks_for[i] and gamemarks_matrix[0][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
         if gamemarks_matrix[1][0] == gamemarks_for[i] and gamemarks_matrix[1][1] == gamemarks_for[i] and gamemarks_matrix[1][2] == gamemarks_for[i]: return(False, gamemarks_for[i])
